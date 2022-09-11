bc3b92ae2c82789152406d505eca5760dd9ce8db
Try Ajitiono <ballinst@gmail.com>
2022-09-11 09:03:05 +0700

    add build gh-pages

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import solid from '@astrojs/solid-js';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solid(), tailwind()]
+  integrations: [solid(), tailwind()],
+  vite: {
+    base: process.env.BASE_PATH
+  }
 });
