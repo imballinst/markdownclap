0f621d3ab1411f34a31911927edda29b6dd290fd
Try Ajitiono <ballinst@gmail.com>
2022-10-12 15:02:36 +0000

    add toolbar headings, played around undefined window

--- src/components/Editor/Toolbar/HeadingToolbarButton.tsx
+++ src/components/Editor/Toolbar/HeadingToolbarButton.tsx
@@ -1,10 +1,22 @@
-import { createSignal } from 'solid-js';
+import { createSignal, For } from 'solid-js';
 import { JSX } from 'solid-js/jsx-runtime';
+import {
+  HotkeyContainerObject,
+  useClientSideHotkeyHook
+} from '../../../hooks/useClientSideHotkeyHook';
 import { ToolbarAction } from '../../../utils/operators/toolbar';
 import { Popover, PopoverStyleState } from '../../Popover';
-import { getToolbarHoverText } from './common';
 
 import './HeadingToolbarButton.css';
+
+const HOTKEYS: HotkeyContainerObject[] = [
+  { defaultText: `Heading 1`, keys: [`1`], action: ToolbarAction.TOOLBAR_HEADING_1 },
+  { defaultText: `Heading 2`, keys: [`2`], action: ToolbarAction.TOOLBAR_HEADING_2 },
+  { defaultText: `Heading 3`, keys: [`3`], action: ToolbarAction.TOOLBAR_HEADING_3 },
+  { defaultText: `Heading 4`, keys: [`4`], action: ToolbarAction.TOOLBAR_HEADING_4 },
+  { defaultText: `Heading 5`, keys: [`5`], action: ToolbarAction.TOOLBAR_HEADING_5 },
+  { defaultText: `Heading 6`, keys: [`6`], action: ToolbarAction.TOOLBAR_HEADING_6 }
+];
 
 interface HeadingToolbarButtonProps {
   onClick: JSX.DOMAttributes<HTMLButtonElement>['onClick'];
@@ -16,6 +28,8 @@ export function HeadingToolbarButton({ onClick }: HeadingToolbarButtonProps) {
     top: '0px'
   });
   const [isPopoverShown, setIsPopoverShown] = createSignal(false);
+
+  const hotkeys = useClientSideHotkeyHook(HOTKEYS);
 
   return (
     <>
@@ -39,9 +53,17 @@ export function HeadingToolbarButton({ onClick }: HeadingToolbarButtonProps) {
         popoverStyle={popoverStyle}
         className="heading-toolbar-popover"
       >
-        <button class="button-sm" onClick={onClick} data-action={ToolbarAction.TOOLBAR_HEADING_1}>
-          Heading 1 ({getToolbarHoverText(`Heading 1`, [`1`])})
-        </button>
+        <ul class="headings-list">
+          <For each={hotkeys()}>
+            {(hotkey, index) => (
+              <li>
+                <button class="button-sm" onClick={onClick} data-action={HOTKEYS[index()].action}>
+                  {hotkey}
+                </button>
+              </li>
+            )}
+          </For>
+        </ul>
       </Popover>
     </>
   );
