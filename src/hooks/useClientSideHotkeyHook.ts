76a1f6c90d6564663388d63e452b5772a246e449
Try Ajitiono <ballinst@gmail.com>
2022-10-13 19:17:22 +0700

    prettify

--- src/hooks/useClientSideHotkeyHook.ts
+++ src/hooks/useClientSideHotkeyHook.ts
@@ -1,25 +1,29 @@
-import { Accessor, createSignal, onMount } from "solid-js";
-import { getToolbarHoverText } from "../components/Editor/Toolbar/common";
+import { Accessor, createSignal, onMount } from 'solid-js';
+import { getToolbarHoverText } from '../components/Editor/Toolbar/common';
 
 export interface HotkeyContainerObject {
-  defaultText: string
-  keys: string[]
-  action: string
+  defaultText: string;
+  keys: string[];
+  action: string;
 }
 
-export function useClientSideHotkeyHook<T extends HotkeyContainerObject>(defaultValue: T): Accessor<string>;
-export function useClientSideHotkeyHook<T extends HotkeyContainerObject>(defaultValue: T[]): Accessor<string[]>;
+export function useClientSideHotkeyHook<T extends HotkeyContainerObject>(
+  defaultValue: T
+): Accessor<string>;
+export function useClientSideHotkeyHook<T extends HotkeyContainerObject>(
+  defaultValue: T[]
+): Accessor<string[]>;
 
 export function useClientSideHotkeyHook<T extends HotkeyContainerObject>(defaultValue: T | T[]) {
-  const [value, setValue] = createSignal(Array.isArray(defaultValue) ? [] as string[] : '')
+  const [value, setValue] = createSignal(Array.isArray(defaultValue) ? ([] as string[]) : '');
 
   onMount(() => {
     if (Array.isArray(defaultValue)) {
-      setValue(defaultValue.map(item => getToolbarHoverText(item.defaultText, item.keys)))
+      setValue(defaultValue.map((item) => getToolbarHoverText(item.defaultText, item.keys)));
     } else {
-      setValue(getToolbarHoverText(defaultValue.defaultText, defaultValue.keys))
+      setValue(getToolbarHoverText(defaultValue.defaultText, defaultValue.keys));
     }
-  })
+  });
 
-  return value 
+  return value;
 }
