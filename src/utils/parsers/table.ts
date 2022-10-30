4feb9c0153d540b5c8a618df9519170ec7a3057d
Try Ajitiono <ballinst@gmail.com>
2022-10-30 04:27:58 +0000

    fix: fix pasting not working for invalid tables and fix pasting table when there is a trailing newline

--- src/utils/parsers/table.ts
+++ src/utils/parsers/table.ts
@@ -1,10 +1,10 @@
 export function parseTableFromTabbedText(text: string | undefined): string | undefined {
   if (!text) return undefined
 
-  const lines = text.split('\n')  
-  const tabCountPerLine = lines.map(line => line.match(/\t/g)?.length || 0)
+  const lines = text.trim().split('\n')
+  const tabCountPerLine = lines.map(line => line.match(/\t+/g)?.length || 0)
   const allLinesHaveEqualTabCount = tabCountPerLine.every(tabCount => tabCount === tabCountPerLine[0] && tabCount > 0)
 
   if (!allLinesHaveEqualTabCount) return undefined
-  return lines.map(line => `|${line.replace(/\|/g, '\\|').replace(/\t/g, '|')}|`).join('\n')
+  return lines.map(line => `|${line.replace(/\|/g, '\\|').replace(/\t+/g, '|')}|`).join('\n')
 }
